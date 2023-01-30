add3a043a0f4b15b60b4979a821fd084cf8438bb
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-30 22:17:07 +0530

    Create Fruits.js

---
+++ src/components/Fruits.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TypesOfFruits from './TypesOfFruits'
+
+import React, { Component } from 'react'
+
+export default class Fruits extends Component {
+  render() {
+    return (
+      <TypesOfFruits/>
+    )
+  }
+}
